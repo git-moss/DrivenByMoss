b32de5b6cd79610fc7174e7e076822c73ced1fe5
Nicholas Allen <nick@bitwig.com>
2018-05-07 14:50:40 +0200

    Update to work with my latest changes to the API. Now controllers don't need to deal with USB transfer errors themselves. If there is an error then the controller will stop and user will see a notification (the same as we do for MIDI IO).

--- src/main/java/de/mossgrabers/bitwig/framework/usb/USBEndpointImpl.java
+++ src/main/java/de/mossgrabers/bitwig/framework/usb/USBEndpointImpl.java
@@ -4,16 +4,13 @@
 
 package de.mossgrabers.bitwig.framework.usb;
 
+import java.nio.ByteBuffer;
+
+import com.bitwig.extension.controller.api.UsbEndpoint;
+
 import de.mossgrabers.framework.daw.IHost;
 import de.mossgrabers.framework.usb.IUSBAsyncCallback;
 import de.mossgrabers.framework.usb.IUSBEndpoint;
-
-import com.bitwig.extension.controller.api.UsbEndpoint;
-import com.bitwig.extension.controller.api.UsbTransferError;
-import com.bitwig.extension.controller.api.UsbTransferException;
-
-import java.nio.ByteBuffer;
-
 
 /**
  * Implementation for an USB endpoint.
@@ -22,47 +19,37 @@ import java.nio.ByteBuffer;
  */
 public class USBEndpointImpl implements IUSBEndpoint
 {
-    private IHost       host;
-    private UsbEndpoint endpoint;
+   private IHost host;
 
+   private UsbEndpoint endpoint;
 
-    /**
-     * Constructor.
-     *
-     * @param host The host
-     * @param endpoint The Bitwig endpoint
-     */
-    public USBEndpointImpl (final IHost host, final UsbEndpoint endpoint)
-    {
-        this.host = host;
-        this.endpoint = endpoint;
-    }
+   /**
+    * Constructor.
+    *
+    * @param host
+    *           The host
+    * @param endpoint
+    *           The Bitwig endpoint
+    */
+   public USBEndpointImpl(final IHost host, final UsbEndpoint endpoint)
+   {
+      this.host = host;
+      this.endpoint = endpoint;
+   }
 
+   /** {@inheritDoc} */
+   @Override
+   public void send(final ByteBuffer buffer, final int timeout)
+   {
+      this.endpoint.bulkTransfer(buffer, timeout);
+   }
 
-    /** {@inheritDoc} */
-    @Override
-    public void send (final ByteBuffer buffer, final int timeout)
-    {
-        try
-        {
-            this.endpoint.bulkTransfer (buffer, timeout);
-        }
-        catch (final UsbTransferException e)
-        {
-            this.host.error ("USB transmission error: " + e);
-        }
-    }
-
-
-    /** {@inheritDoc} */
-    @Override
-    public void sendAsync (final ByteBuffer buffer, final IUSBAsyncCallback callback, final int timeout)
-    {
-        this.endpoint.asyncBulkTransfer (buffer, result -> {
-            final UsbTransferError error = result.error ();
-            if (error != null)
-                this.host.error ("USB receive error: " + error.getErrorMessage ());
-            callback.process (error == null ? result.actualLength () : -1);
-        }, timeout);
-    }
+   /** {@inheritDoc} */
+   @Override
+   public void sendAsync(final ByteBuffer buffer, final IUSBAsyncCallback callback, final int timeout)
+   {
+      this.endpoint.asyncBulkTransfer(buffer, amountTransferred -> {
+         callback.process(amountTransferred);
+      }, timeout);
+   }
 }
