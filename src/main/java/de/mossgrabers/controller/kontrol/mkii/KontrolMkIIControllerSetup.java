e69020b633274a0ccad70e780f58962bc39a428c
Alexandre Bique <bique.alexandre@gmail.com>
2019-05-08 18:31:22 +0200

    Show a notification if the device parameter are not configured for Komplete Kontrol

--- src/main/java/de/mossgrabers/controller/kontrol/mkii/KontrolMkIIControllerSetup.java
+++ src/main/java/de/mossgrabers/controller/kontrol/mkii/KontrolMkIIControllerSetup.java
@@ -376,8 +376,12 @@ public class KontrolMkIIControllerSetup extends AbstractControllerSetup<KontrolM
     private String getKompleteInstance ()
     {
         final ICursorDevice instrumentDevice = this.model.getInstrumentDevice ();
-        if (instrumentDevice.doesExist () && instrumentDevice.getName ().startsWith ("Komplete Kontrol"))
-            return instrumentDevice.getParameterBank ().getItem (0).getName ();
+        if (instrumentDevice.doesExist () && instrumentDevice.getName ().startsWith ("Komplete Kontrol")) {
+            final String name = instrumentDevice.getParameterBank().getItem(0).getName();
+            if (name.isEmpty())
+                host.showNotification("You need to map the first parameter of the Komplete plugin to the first page parameter of the Bitwig remote ");
+            return name;
+        }
         return "";
     }
 
