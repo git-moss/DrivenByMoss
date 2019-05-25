9a9dfad9b9005dcd8a349d7ec88acf87dc1fca18
git-moss <juergen.mossgraber@googlemail.com>
2019-05-25 15:36:43 +0200

    * Requires Bitwig API 9. * OSC   * New: The port of the OSC server ("receive on") can be configured again. * Push 1/2   * New: Delete + Scene button (in Play, Piano, Drum 64, Session view): Deletes the scene   * New: Delete + pad in scene play mode: Deletes the scene

---
+++ src/main/java/de/mossgrabers/bitwig/framework/osc/OpenSoundControlServerImpl.java
@@ -0,0 +1,41 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+// (c) 2017-2019
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+package de.mossgrabers.bitwig.framework.osc;
+
+import de.mossgrabers.framework.osc.IOpenSoundControlServer;
+
+import com.bitwig.extension.api.opensoundcontrol.OscServer;
+
+import java.io.IOException;
+
+
+/**
+ * Implementation of an OSC server.
+ *
+ * @author J&uuml;rgen Mo&szlig;graber
+ */
+public class OpenSoundControlServerImpl implements IOpenSoundControlServer
+{
+    private final OscServer server;
+
+
+    /**
+     * Constructor.
+     * 
+     * @param server The Bitwig OSC server implementation
+     */
+    public OpenSoundControlServerImpl (final OscServer server)
+    {
+        this.server = server;
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void start (final int port) throws IOException
+    {
+        this.server.start (port);
+    }
+}
---
+++ src/main/java/de/mossgrabers/framework/osc/IOpenSoundControlClient.java
@@ -0,0 +1,34 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+// (c) 2017-2019
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+package de.mossgrabers.framework.osc;
+
+import java.io.IOException;
+import java.util.List;
+
+
+/**
+ * Interface for sending messages to an OSC server.
+ *
+ * @author J&uuml;rgen Mo&szlig;graber
+ */
+public interface IOpenSoundControlClient
+{
+    /**
+     * Send a message to the server.
+     *
+     * @param message The message to send
+     * @throws IOException Could not send the message
+     */
+    void sendMessage (IOpenSoundControlMessage message) throws IOException;
+
+
+    /**
+     * Send several messages to the server as an OSC bundle.
+     *
+     * @param messages The messages to send
+     * @throws IOException Could not send the messages
+     */
+    void sendBundle (List<IOpenSoundControlMessage> messages) throws IOException;
+}
