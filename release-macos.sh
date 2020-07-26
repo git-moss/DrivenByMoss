1fd4bef65e5ffb56d84aa38b74629d850be4c10d
mos <mos@moss-mac-mini.fritz.box>
2020-07-26 21:08:55 +0200

    Updated JDK path.

--- release-macos.sh
+++ release-macos.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-12-openj9.jdk/Contents/Home
 mvn clean install package -Dbitwig.extension.directory=target
