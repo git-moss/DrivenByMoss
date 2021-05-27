51f8046e4c82429997cf20c8508e3184976ae679
git-moss <juergen.mossgraber@googlemail.com>
2021-05-27 23:54:14 +0200

    Switched API to 14. Updated some Maven plugins.

--- src/main/java/de/mossgrabers/bitwig/framework/extension/AbstractControllerExtensionDefinition.java
+++ src/main/java/de/mossgrabers/bitwig/framework/extension/AbstractControllerExtensionDefinition.java
@@ -111,7 +111,7 @@ public abstract class AbstractControllerExtensionDefinition extends ControllerEx
     @Override
     public int getRequiredAPIVersion ()
     {
-        return 12;
+        return 14;
     }
 
 
