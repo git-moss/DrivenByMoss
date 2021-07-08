59e5c3c047f1965649c5b57943a51576588e37ad
git-moss <juergen.mossgraber@googlemail.com>
2021-07-08 20:37:30 +0200

    * Fire   * New: Scene 3+Pad in Drum 64 mode soloes the drum sound.   * New: Scene 4+Pad in Drum 64 mode mutes the drum sound.

--- src/main/java/de/mossgrabers/controller/akai/fire/view/DrumView64.java
+++ src/main/java/de/mossgrabers/controller/akai/fire/view/DrumView64.java
@@ -6,6 +6,7 @@ package de.mossgrabers.controller.akai.fire.view;
 
 import de.mossgrabers.controller.akai.fire.FireConfiguration;
 import de.mossgrabers.controller.akai.fire.controller.FireControlSurface;
+import de.mossgrabers.framework.controller.ButtonID;
 import de.mossgrabers.framework.daw.IModel;
 import de.mossgrabers.framework.view.AbstractDrum64View;
 
@@ -15,7 +16,7 @@ import de.mossgrabers.framework.view.AbstractDrum64View;
  *
  * @author J&uuml;rgen Mo&szlig;graber
  */
-public class DrumView64 extends AbstractDrum64View<FireControlSurface, FireConfiguration>
+public class DrumView64 extends AbstractDrum64View<FireControlSurface, FireConfiguration> implements IFireView
 {
     /**
      * Constructor.
@@ -26,5 +27,69 @@ public class DrumView64 extends AbstractDrum64View<FireControlSurface, FireConfi
     public DrumView64 (final FireControlSurface surface, final IModel model)
     {
         super (surface, model, 16, 4);
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    protected void handleButtonCombinations (final int playedPad)
+    {
+        if (this.surface.isPressed (ButtonID.SCENE3))
+            this.handleSoloButton (playedPad);
+        else if (this.surface.isPressed (ButtonID.SCENE4))
+            this.handleMuteButton (playedPad);
+
+        super.handleButtonCombinations (playedPad);
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public int getButtonColor (final ButtonID buttonID)
+    {
+        switch (buttonID)
+        {
+            case SCENE3:
+                return this.surface.isPressed (ButtonID.SCENE3) ? 2 : 1;
+            case SCENE4:
+                return this.surface.isPressed (ButtonID.SCENE4) ? 2 : 1;
+
+            case SCENE1:
+            case SCENE2:
+            default:
+                return 0;
+        }
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public int getSoloButtonColor (final int index)
+    {
+        switch (index)
+        {
+            case 2:
+                return 4;
+
+            case 3:
+                return 1;
+
+            case 0:
+            case 1:
+            default:
+                return 0;
+        }
+    }
+
+
+    /** {@inheritDoc} */
+    @Override
+    public void onSelectKnobValue (final int value)
+    {
+        if (this.model.getValueChanger ().isIncrease (value))
+            this.scales.incDrumOctave ();
+        else
+            this.scales.decDrumOctave ();
+        this.updateNoteMapping ();
     }
 }
