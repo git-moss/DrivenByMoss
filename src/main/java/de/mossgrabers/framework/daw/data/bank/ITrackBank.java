fd28fb42c05227ffd448c46b6810a7442dfd65d3
git-moss <juergen.mossgraber@googlemail.com>
2021-08-30 12:07:06 +0200

    Code improvements. POM improvements.

--- src/main/java/de/mossgrabers/framework/daw/data/bank/ITrackBank.java
+++ src/main/java/de/mossgrabers/framework/daw/data/bank/ITrackBank.java
@@ -5,10 +5,13 @@
 package de.mossgrabers.framework.daw.data.bank;
 
 import de.mossgrabers.framework.controller.color.ColorEx;
+import de.mossgrabers.framework.daw.data.IDeviceMetadata;
 import de.mossgrabers.framework.daw.data.ITrack;
 import de.mossgrabers.framework.daw.resource.ChannelType;
 import de.mossgrabers.framework.observer.IIndexedValueObserver;
 import de.mossgrabers.framework.observer.INoteObserver;
+
+import java.util.List;
 
 
 /**
@@ -101,4 +104,14 @@ public interface ITrackBank extends IChannelBank<ITrack>
      * @param color The color of the channel, might be null
      */
     void addChannel (ChannelType type, String name, ColorEx color);
+
+
+    /**
+     * Adds a new channel to this channel bank.
+     *
+     * @param type The type of channel to add
+     * @param name The name of the channel, might be null
+     * @param devices The devices to add to the channel
+     */
+    void addChannel (ChannelType type, String name, List<IDeviceMetadata> devices);
 }
