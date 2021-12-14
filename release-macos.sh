133e583f654a18efa2ecaa9962ec85203046b9cf
Jürgen Moßgraber <mos@moss-mac-mini.fritz.box>
2021-12-14 20:10:49 +0100

    Updated JDK 17 path.

--- release-macos.sh
+++ release-macos.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home
 mvn clean install package -Dbitwig.extension.directory=target
