f7759365f09cc3425bb2e6463e740031d28a0a53
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-08-13 11:40:39 +0200

    use the java version from the user home by default or the debian system one (could be adapted to other distributions)

--- release-linux.sh
+++ release-linux.sh
@@ -1,2 +1,7 @@
-export JAVA_HOME=~/java/jdk-17.0.5+8
+if [ -d "/home/$USER/java/jdk-17.0.5+8" ]; then
+    export JAVA_HOME=/home/$USER/java/jdk-17.0.5+8
+else
+    export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+fi
+
 mvn clean install package -Dbitwig.extension.directory=target
