a2472ca8a393543123d0e407edb88237f0df5cc4
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-08-13 15:10:20 +0200

    add variables to manage user and debian Java homes

--- release-linux.sh
+++ release-linux.sh
@@ -1,7 +1,10 @@
-if [ -d "/home/$USER/java/jdk-17.0.5+8" ]; then
-    export JAVA_HOME=/home/$USER/java/jdk-17.0.5+8
-else
-    export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+JAVA_HOME_USER=/home/$USER/java/jdk-17.0.5+8
+JAVA_HOME_DEBIAN=/usr/lib/jvm/java-17-openjdk-amd64
+
+if [ -d $JAVA_HOME_USER ]; then
+    export JAVA_HOME=$JAVA_HOME_USER
+elif [ -d $JAVA_HOME_DEBIAN ]; then
+    export JAVA_HOME=$JAVA_HOME_DEBIAN
 fi
 
 mvn clean install package -Dbitwig.extension.directory=target
