9691d4928c8b3a1fd8d6ab5c9207f9a67b7b0b52
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2026-01-06 14:31:21 +0100

    Happy new year.

--- src/main/java/de/mossgrabers/controller/ni/kontrol/mki/mode/ScaleMode.java
+++ src/main/java/de/mossgrabers/controller/ni/kontrol/mki/mode/ScaleMode.java
@@ -1,5 +1,5 @@
 // Written by Jürgen Moßgraber - mossgrabers.de
-// (c) 2017-2025
+// (c) 2017-2026
 // Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
 
 package de.mossgrabers.controller.ni.kontrol.mki.mode;
