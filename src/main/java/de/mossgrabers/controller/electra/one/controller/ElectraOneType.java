fa55b99419c5bd3a496bef86e5e21f4ab2396ef3
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2026-04-10 16:13:40 +0200

    Added missing file.

---
+++ src/main/java/de/mossgrabers/controller/electra/one/controller/ElectraOneType.java
@@ -0,0 +1,20 @@
+// Written by Jürgen Moßgraber - mossgrabers.de
+// (c) 2017-2026
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
+
+package de.mossgrabers.controller.electra.one.controller;
+
+/**
+ * The specific type of the Electra One controller series.
+ *
+ * @author Jürgen Moßgraber
+ */
+public enum ElectraOneType
+{
+    /** The Electra One Mk1. */
+    MK1,
+    /** The Electra One Mk2. */
+    MK2,
+    /** The Electra One Mini. */
+    MINI
+}
